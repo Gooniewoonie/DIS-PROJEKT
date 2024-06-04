242e8df47e435bf8ef912e2f352d77aa8d9f55f1
Gooniewoonie <155816349+Gooniewoonie@users.noreply.github.com>
2024-06-04 11:24:15 +0200

    Add files via upload

---
+++ DISapp/forms.py
@@ -0,0 +1,27 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from appDIS import select_user 
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=30)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = select_user(username.data)
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        # TODO: Use same logic as valiudate username iguess.
+        pass
+
+class UserLoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
---
+++ DISapp/models.py
@@ -0,0 +1,59 @@
+from datetime import datetime
+from appDIS import conn, login_manager
+from flask_login import UserMixin
+from psycopg2 import sql
+
+@login_manager.user_loader
+def load_user(user_id):
+    cur = conn.cursor()
+    cur.execute("""
+    SELECT * FROM users
+    WHERE id = %s
+    """, (int(user_id),))
+    if cur.rowcount > 0:
+        return User(cur.fetchone())
+    else:
+        return None
+
+class User(UserMixin):
+    def __init__(self, user_data):
+        self.id = user_data[0]
+        self.username = user_data[1]
+        self.email = user_data[2]
+        self.password = user_data[3]
+        self.role = user_data[4]
+
+    def get_id(self):
+       return self.id
+
+def insert_user(username, email, password, role):
+    cur = conn.cursor()
+    sql = """
+    INSERT INTO users(username, email, password, role)
+    VALUES (%s, %s, %s, %s)
+    """
+    cur.execute(sql, (username, email, password, role))
+    conn.commit()
+    cur.close()
+
+def select_user(username):
+    cur = conn.cursor()
+    sql = """
+    SELECT * FROM users
+    WHERE username = %s
+    """
+    cur.execute(sql, (username,))
+    user = User(cur.fetchone()) if cur.rowcount > 0 else None
+    cur.close()
+    return user
+
+def select_user_by_id(user_id):
+    cur = conn.cursor()
+    sql = """
+    SELECT * FROM users
+    WHERE id = %s
+    """
+    cur.execute(sql, (user_id,))
+    user = User(cur.fetchone()) if cur.rowcount > 0 else None
+    cur.close()
+    return user
